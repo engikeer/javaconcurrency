8cb19062730aaf39ef7d0db98b12c86cb3189912
engikeer <engikeer@github.com>
2019-11-17 22:51:16 +0800

    Update

---
+++ src/main/java/ServletTest.java
@@ -0,0 +1,30 @@
+import javax.servlet.*;
+
+public class ServletTest implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service (ServletRequest request, ServletResponse response) {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+
+}
