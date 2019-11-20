af17f3eafeceab5da9ed405ee5be965abd3614d9
engikeer <1HandHAND>
2019-11-20 21:58:54 +0800

    Update

---
+++ src/main/java/SynchronizedDemo.java
@@ -0,0 +1,46 @@
+public class SynchronizedDemo {
+    private boolean ready = false;
+    private int result = 0;
+    private int number = 1;
+
+    private void write() {
+        ready = true;
+        number = 2;
+    }
+
+    private void read() {
+        if (ready) {
+            result = number * 3;
+        }
+        System.out.println("result = " + result);
+    }
+
+    public static void main(String[] args) {
+        SynchronizedDemo sd = new SynchronizedDemo();
+        Thread readThread = new Thread(sd::read);
+        Thread writeThread = new Thread(sd::write);
+        writeThread.start();
+        readThread.start();
+    }
+//    private class CThread extends Thread {
+//        private boolean flag;
+//        public CThread(boolean flag) {
+//            this.flag = flag;
+//        }
+//
+//        @Override
+//        public void run() {
+//            if (flag) {
+//                write();
+//            } else {
+//                read();
+//            }
+//        }
+//    }
+
+//    public static void main(String[] args) {
+//        SynchronizedDemo sd = new SynchronizedDemo();
+//        sd.new CThread(true).start();
+//        sd.new CThread(false).start();
+//    }
+}
